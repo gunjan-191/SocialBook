0325a537d04f3c99fe94f2a568177d7f8cafe70d
Gunjan Sachdeva <gunjan.sachdeva@lyxelandflamingo.com>
2024-02-26 21:11:10 +0530

    account_setting_page

--- core/views.py
+++ core/views.py
@@ -3,13 +3,44 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
-def signup(request):
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    print(user_profile)    
+
     if request.method == "POST":
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+
+def signup(request):
+
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
@@ -17,21 +48,50 @@ def signup(request):
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request, "Email exist.")
+                messages.info(request, 'Email Taken')
                 return redirect('signup')
-            elif User.objects.filter(username = username).exists():
-                messages.info(request, "Username exist")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password= password)
+                user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                user_model = User.objects.get(username = username)
-                new_profile = Profile.objects.create(user= user_model, id_user = user_model.id)
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
-            messages.info(request, "Password not matching")
+            messages.info(request, 'Password Not Matching')
             return redirect('signup')
+        
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
